46b616a424f0a224c8d4004f6e39da6a68550ba5
tanvir243 <travelertanvir@gmail.com>
2024-02-01 10:54:52 +0600

    odd-even task completed

---
+++ solution/while/odd-even.js
@@ -0,0 +1,18 @@
+
+// Subtask-1
+let num = 61;
+while(num <= 100){
+    if(num % 2 === 1){
+        console.log(num);
+    }
+    num++;
+}
+
+// Subtask-2
+let num = 78;
+while(num <= 98){
+    if(num % 2 === 0){
+        console.log(num);
+    }
+    num++;
+} 

bc14b6769da84ec9c261fa3dd030c43fe63335cb
tanvir243 <travelertanvir@gmail.com>
2024-02-01 11:46:05 +0600

    odd-even number completed

---
+++ solution/for/2.odd-even.js
@@ -0,0 +1,14 @@
+
+// subtask-1
+for(let i = 61; i <= 100; i++){
+    if(i % 2 === 1){
+        console.log(i);
+    }
+}
+
+// subtask-2
+for(let i = 78; i <= 98; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}

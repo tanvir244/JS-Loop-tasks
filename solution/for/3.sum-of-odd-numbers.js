d970cb5f19eba4d69d5c997323f178c6c7bd287a
tanvir243 <travelertanvir@gmail.com>
2024-02-01 11:54:52 +0600

    sum-of-odd-numbers completed

---
+++ solution/for/3.sum-of-odd-numbers.js
@@ -0,0 +1,20 @@
+
+// subtask-1
+let sum = 0;
+for(let i = 91; i <= 129; i++){
+    if(i % 2 === 1){
+        console.log(i);
+        sum = sum + i;
+        console.log("total", sum);
+    }
+}
+
+// subtask-2
+let sum = 0;
+for(let i = 51; i <= 85; i++){
+    if(i % 2 === 0){
+        console.log(i);
+        sum = sum + i;
+        console.log("total:", sum);
+    }
+}

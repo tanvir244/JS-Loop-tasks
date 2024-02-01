0eace9b7703bd0ba896e0b2d38c85f94c8692be2
tanvir243 <travelertanvir@gmail.com>
2024-02-01 11:04:21 +0600

    sum-of-odd-number completed

---
+++ solution/while/sum-of-odd-numbers.js
@@ -0,0 +1,24 @@
+
+// subtask-1
+let num = 81;
+let sum = 0;
+while(num <= 131){
+    if(num % 2 === 1){
+        console.log(num);
+        sum = sum + num;
+        console.log("total:", sum);
+    }
+    num++;
+}
+
+// subtask-2
+let num = 206;
+let sum = 0;
+while(num <= 311){
+    if(num % 2 === 0){
+        console.log(num);
+        sum = sum + num;
+        console.log("total", sum);
+    }
+    num++;
+}

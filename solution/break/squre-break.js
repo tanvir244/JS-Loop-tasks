51a1f09133334acc35c0980d9f26a3182ed4d533
tanvir243 <travelertanvir@gmail.com>
2024-02-01 12:49:26 +0600

    squre-break task completed

---
+++ solution/break/squre-break.js
@@ -0,0 +1,26 @@
+// first squre number : root of 4 = 2
+for(let i = 1; i <= 100; i++){
+    if(Math.sqrt(i) === 2){
+        console.log("First Square Number:", i);
+        break;
+    }
+    console.log(i);
+}
+
+// second squre number : root of 9 = 3
+for(let i = 1; i <= 100; i++){
+    if(Math.sqrt(i) === 3){
+        console.log("Second Square Number:", i);
+        break;
+    }
+    console.log(i);
+}
+
+// third squre number : root of 16 = 4
+for(let i = 1; i <= 100; i++){
+    if(Math.sqrt(i) === 4){
+        console.log("Third Square Number:", i);
+        break;
+    }
+    console.log(i);
+}

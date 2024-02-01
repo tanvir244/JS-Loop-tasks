a11102284df60af961653385432fe7d0b4968bc0
tanvir243 <travelertanvir@gmail.com>
2024-02-01 11:16:55 +0600

    multiplication table completed

---
+++ solution/while/multiplication-table.js
@@ -0,0 +1,9 @@
+
+// multiplication table for number - 5
+let multiplication = 5;
+let num = 1;
+while(num <= 10){
+    let table = `${multiplication} * ${num} = ${multiplication * num}`;
+    console.log(table);
+    num++;
+}
